e0b7a54f39a854802166410701ff54cb1f322d30
Dan Proctor <dproc96@gmail.com>
2020-08-14 16:28:59 -0400

    Added descriptions and fonts

--- client/src/components/Builder.js
+++ client/src/components/Builder.js
@@ -26,6 +26,14 @@ class Builder extends React.Component {
             dc: 2,
             save: 2
         }
+        this.descriptions = {
+            ac: "If you raise the AC, consider lowering the HP or Offense",
+            attack: "This is the attack bonus for all attacks. If you raise it, consider lowering defense or damage",
+            dc: "This is the save DC for any effects of this creature",
+            save: "This is the bonus for the creature's best saving throw, others should be lower or equal to this",
+            hp: "If you raise HP, consider lowering AC or Offense",
+            damage: "This is the damage pool for the creature's entire turn, if you increase this, consider lowering the attack bonus or Defense"
+        }
     }
 
     handleChange = (e) => {
@@ -54,13 +62,15 @@ class Builder extends React.Component {
 
     handleSubmit = () => {
         const challenge = this.state.challenge;
-        const monster = new Monster(challenge);
-        this.setState({
-            monster: {
-                stats: monster.stats,
-                baseStats: {...monster.stats}
-            }
-        })
+        if (challenge >= 0 || ["1/8", "1/4", "1/2"].indexOf(challenge) > -1) {
+            const monster = new Monster(challenge);
+            this.setState({
+                monster: {
+                    stats: monster.stats,
+                    baseStats: {...monster.stats}
+                }
+            })
+        }
     }
 
     handleBack = () => {
@@ -77,17 +87,19 @@ class Builder extends React.Component {
 
     render() {
         if (this.state.monster) {
+            const stats = Object.keys(this.state.monster.stats).map((key) => {
+                return (
+                    <div>
+                        <h3 key={key}><strong>{key.toUpperCase()}: </strong>{this.state.monster.stats[key]}</h3>
+                        <input onChange={this.handleChange} type="range" key={key + "Slider"} name={key + "Slider"} id={key} value={this.state[key + "Slider"]} />
+                        <p>{this.descriptions[key]}</p>
+                    </div>
+                )
+            })
             return (
                 <Box>
                     <h2>New CR {this.state.challenge} Monster</h2>
-                    {Object.keys(this.state.monster.stats).map((key) => {
-                        return (
-                            <div>
-                                <p key={key}><strong>{key.toUpperCase()}: </strong>{this.state.monster.stats[key]}</p>
-                                <input onChange={this.handleChange} type="range" key={key + "Slider"} name={key + "Slider"} id={key} value={this.state[key+"Slider"]} />
-                            </div>
-                        )
-                    })}
+                    {stats}
                     <Button onClick={this.handleBack}>Back</Button>
                 </Box>
             )
@@ -95,7 +107,8 @@ class Builder extends React.Component {
         return (
             <Box>
                 <h2>Enter a Challenge Rating</h2>
-                <input onChange={this.handleChange} id="challenge" name="challenge" value={this.state.challenge}></input>
+                <p>This could be a positive whole number, 0, 1/8, 1/4, or 1/2</p>
+                <input style={{textAlign: "center", padding: 5}} onChange={this.handleChange} id="challenge" name="challenge" value={this.state.challenge}></input>
                 <Button onClick={this.handleSubmit}>Submit</Button>
             </Box>
         )

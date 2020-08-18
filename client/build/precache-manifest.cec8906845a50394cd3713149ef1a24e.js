ae8664ad2e038cbad79e7d85be79ed526b493a88
Dan Proctor <dproc96@gmail.com>
2020-08-17 20:17:37 -0400

    Minor touches for deploy to firebase

---
+++ client/build/precache-manifest.cec8906845a50394cd3713149ef1a24e.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "48025b009bd3a953d2c9",
+    "url": "/static/js/main.506a3e74.chunk.js"
+  },
+  {
+    "revision": "cc26138bd66a38210ad5",
+    "url": "/static/js/2.e4069f75.chunk.js"
+  },
+  {
+    "revision": "cc205d3457ae679d29fbb119941138fa",
+    "url": "/index.html"
+  }
+];
